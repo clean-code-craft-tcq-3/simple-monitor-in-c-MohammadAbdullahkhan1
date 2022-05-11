147d223fda56b866ea9cc4cb945d3b57c5def6e3
MohammadAbdullahkhan1 <100206902+MohammadAbdullahkhan1@users.noreply.github.com>
2022-05-11 23:14:49 +0530

    Create checkRange.c

---
+++ checkRange.c
@@ -0,0 +1,28 @@
+#include "header.h"
+
+int count=0;
+
+int isChargeRateInRange(float chargeRate)
+{
+  chargeRate > 0.8?count:count++
+  return count;
+}
+
+int isSocInRange(float soc)
+{
+    if(soc < 20 || soc > 80) 
+    {
+      count++;
+    }
+    return count;
+}
+
+int isTemperatureInRange(float temperature)
+{
+    Std_ReturnType RetVal = E_OK;
+    if(temperature < 0 || temperature > 45) 
+    {
+      count++;
+    }
+    return count;
+}
